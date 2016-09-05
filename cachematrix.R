a51b16ca521f38b6a4c9faab4f4b59b551056a4d
NR <nmum86@gmail.com>
2016-09-04 21:21:47 -0400

    Functions to Create and Store Inverse of a Matrix
    ## First created a 3*3 matrix 
    How I tested the functions:
    ================
    > x = rbind (c(5, 3, -8), c(4, 2, 3), c(6, 8, 5)
    + )
    > ## displaying x
    > x
         [,1] [,2] [,3]
    [1,]    5    3   -8
    [2,]    4    2    3
    [3,]    6    8    5
    > ##  loading x through makeCacheMatrix
    > lm = makeCacheMatrix(x)
    > lm$get()
         [,1] [,2] [,3]
    [1,]    5    3   -8
    [2,]    4    2    3
    [3,]    6    8    5
    > ## Loaded into variable lm
    > ## checking if there is any inverse data
    > lm$ginv()
    NULL
    > ## Nothing by way of inverse data
    > ## Let us "solve" for inverse
    > cacheSolve(lm)
                 [,1]        [,2]         [,3]
    [1,]  0.059322034  0.33474576 -0.105932203
    [2,]  0.008474576 -0.30932203  0.199152542
    [3,] -0.084745763  0.09322034  0.008474576
    > ##let's check for inverse data
    > lm$ginv()
                 [,1]        [,2]         [,3]
    [1,]  0.059322034  0.33474576 -0.105932203
    [2,]  0.008474576 -0.30932203  0.199152542
    [3,] -0.084745763  0.09322034  0.008474576
    > ## It's loaded for the first time into memory 
    > ## now if we run cachesolve again - it should just get from memory
    > cacheSolve(lm)
    getting cached data.
                 [,1]        [,2]         [,3]
    [1,]  0.059322034  0.33474576 -0.105932203
    [2,]  0.008474576 -0.30932203  0.199152542
    [3,] -0.084745763  0.09322034  0.008474576
    > ##Let's run it once more and check - it should still retrieve it from memory
    > cachesolve(lm)
    Error: could not find function "cachesolve"
    > ## didn't type the function name correctly
    > cacheSolve(lm)
    getting cached data.
                 [,1]        [,2]         [,3]
    [1,]  0.059322034  0.33474576 -0.105932203
    [2,]  0.008474576 -0.30932203  0.199152542
    [3,] -0.084745763  0.09322034  0.008474576
    > ##again retrieved cached data -- it works!!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit
+## to caching the inverse of a matrix rather than compute it repeatedly. The
+## following two functions are used to cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list containing a function to
+## 1. sets the value of the matrix
+## 2. gets the value of the matrix
+## 3. sets the value of inverse of the matrix
+## 4. gets the value of inverse of the matrix
+
+## Created - 9/4/2016
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    ## Sets the value of matrix
+    set <- function(y) {                
+        x <<- y
+        inv <<- NULL
+    }
+    ## gets the value of the matrix
+    get <- function() x
+    ## Sets the value of the inverse of the matrix
+    sinv <- function(inverse) inv <<- inverse
+    ## gets the value of the inverse of the matrix
+    ginv <- function() inv
+    list(set=set, get=get, sinv=sinv, ginv=ginv)
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$ginv()
+    ## if already in cache - returns cached information
+    if(!is.null(inv)) {
+        message("getting cached data.")
+        return(inv)
+    }
+    data <- x$get()
+    ## if not in memory computes the inverse in memory
+    inv <- solve(data)
+    x$sinv(inv)
+    inv
 }
